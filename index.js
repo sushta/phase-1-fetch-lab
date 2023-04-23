7a4637997edc2fcb4e0663ee83ac6d47d6730aec
sustapakhreen <sushta48@gmail.com>
2023-04-23 18:04:26 -0500

    completed fetch lab

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  const url = "https://anapioficeandfire.com/api/books"
+   return  fetch(url)
+    .then(r=>r.json() /**/)
+    .then(actualFreakingData=>{
+      renderBooks(actualFreakingData)
+    })
+    
+  }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
